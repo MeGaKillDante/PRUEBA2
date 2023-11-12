98f4fcd44883b2afc355f2da99ba889df797e860
jojosolod0s <jojosolod0s@DESKTOP-5CMH1GH>
2023-11-12 03:52:23 -0500

    print("wenas"

--- src/main/java/com/mycompany/pruebagit/PRUEBAGIT.java
+++ src/main/java/com/mycompany/pruebagit/PRUEBAGIT.java
@@ -13,5 +13,6 @@ public class PRUEBAGIT {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("OLA");
+        System.out.println("wenas");
     }
 }
